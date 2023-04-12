f57f44428eb54804f1704f381f6d31e6e8b63cfd
RoDaThanh <117510172+RoDaThanh@users.noreply.github.com>
2023-04-12 22:01:51 +0700

    testing with python language

---
+++ test.py
@@ -0,0 +1,9 @@
+videoTitle = "Lâu lắm không mặc quả áo học sinh quốc dân quay  df toptop 😆/Dc:tag ☝🏻#tuilathouthou #tiktokdanceVN #fyp #viral #trending #xuhuong #hocsinh #lecirque"
+print(len(videoTitle))
+if videoTitle:
+   if len(videoTitle) >100:
+      videoTitle = videoTitle[0:98]
+   print(videoTitle)
+   print(len(videoTitle))
+else:
+    print(videoTitle)
